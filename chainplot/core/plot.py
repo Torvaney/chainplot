a186ed6583dad6aefcdea055961e2e080729c6c5
ben.torvaney <ben.torvaney@footballradar.com>
2016-10-07 11:28:38 +0100

    updated dict_tools

--- chainplot/core/plot.py
+++ chainplot/core/plot.py
@@ -7,6 +7,7 @@ import scipy.optimize as op
 from adjustText import adjust_text
 
 import chainplot.core.style as plot_style
+from chainplot.utils.dict_tools import replace_dict, split_kwargs, britishdict, combine_dict
 
 
 # NOTES
@@ -40,60 +41,6 @@ def facet_dimensions(number_of_plots):
         ncols += 1
 
     return nrows, ncols
-
-
-def replace_dict(input_dict, val=None, replacement=''):
-    output_dict = input_dict.copy()
-    for k, v in output_dict.items():
-        if v is val:
-            output_dict[k] = replacement
-    return output_dict
-
-
-def split_kwargs(kwargs_dict, prefix='shadow_'):
-    input_dict = kwargs_dict.copy()
-    output_dict = {}
-    shadow_dict = {}
-
-    for k, v in input_dict.items():
-        if bool(re.match(prefix + '.*', k)):
-            shadow_k = re.sub(prefix, '', k)
-            shadow_dict[shadow_k] = v
-        else:
-            output_dict[k] = v
-
-    return output_dict, shadow_dict
-
-
-def to_kwargs(**kwargs):
-    return kwargs
-
-
-def britishdict(argdict):
-    outdict = argdict.copy()
-
-    # Fix awful americanisms
-    for key, value in outdict.items():
-        if bool(re.match('.*colour.*', key)):
-            amerikey = re.sub('colour', 'color', key)
-            outdict[amerikey] = value
-            outdict.pop(key, 0)
-
-    return outdict
-
-
-def combine_dict(a, b):
-    c = a.copy()
-    try:
-        for key, val in b.items():
-            if type(val) == dict:
-                c[key] = combine_dict(a[key], b[key])
-            else:
-                c[key] = val
-    except AttributeError:  # In case oth isn't a dict
-        return NotImplemented
-
-    return c
 
 
 class Plot:
--- chainplot/utils/dict_tools.py
+++ chainplot/utils/dict_tools.py
@@ -1,4 +1,8 @@
+import re
+
+
 def replace_dict(input_dict, val=None, replacement=''):
+    """ Search and replace dict values """
     output_dict = input_dict.copy()
     for k, v in output_dict.items():
         if v is val:
@@ -7,13 +11,20 @@ def replace_dict(input_dict, val=None, replacement=''):
 
 
 def split_kwargs(kwargs_dict, prefix='shadow_'):
+    """
+    Splits dictionary into two new dicts by checking the keys for a given prefix. Those key-value pairs
+     with the prefix will be added to a new dictionary with prefix removed from the keys.
+    :param kwargs_dict: original dict to be checked and split. As dict
+    :param prefix: prefix to search keys for. As string
+    :return: copy of original dict with preficed keys removed, new dict based on prefixed keys
+    """
     input_dict = kwargs_dict.copy()
     output_dict = {}
     shadow_dict = {}
 
     for k, v in input_dict.items():
-        if bool(re.match(prefix + '.*', k)):
-            shadow_k = re.sub(prefix, '', k)
+        if bool(re.match(prefix + '.*', k)):  # find keys with prefix
+            shadow_k = re.sub(prefix, '', k)  # remove prefix
             shadow_dict[shadow_k] = v
         else:
             output_dict[k] = v
@@ -26,20 +37,27 @@ def to_kwargs(**kwargs):
 
 
 def britishdict(argdict):
+    """
+    Creates a new duplicate dict in which keys with british spellings are convered to american spellings.
+    """
     outdict = argdict.copy()
 
-    # Fix awful americanisms
-    if 'colour' in outdict.keys():
-        outdict['color'] = outdict['colour']
-        outdict.pop('colour', 0)
-    if 'edgecolour' in outdict.keys():
-        outdict['edgecolor'] = outdict['edgecolour']
-        outdict.pop('edgecolour', 0)
+    for key, value in outdict.items():
+        if bool(re.match('.*colour.*', key)):
+            amerikey = re.sub('colour', 'color', key)
+            outdict[amerikey] = value
+            outdict.pop(key, 0)
 
     return outdict
 
 
 def combine_dict(a, b):
+    """
+    Combine two dicts into a new dict, using the second dict's values for keys appearing in both dicts
+    :param a: First dict
+    :param b: Second dict
+    :return: dictionary with combined key-value pairs
+    """
     c = a.copy()
     try:
         for key, val in b.items():
@@ -47,7 +65,7 @@ def combine_dict(a, b):
                 c[key] = combine_dict(a[key], b[key])
             else:
                 c[key] = val
-    except AttributeError:  # In case oth isn't a dict
+    except AttributeError:  # In case other isn't a dict
         return NotImplemented
 
     return c
