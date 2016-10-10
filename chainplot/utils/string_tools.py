8d6210d867c0f240410027c3c31ce8938386a339
ben.torvaney <ben.torvaney@footballradar.com>
2016-10-10 17:18:48 +0100

    updated prettify

--- chainplot/utils/string_tools.py
+++ chainplot/utils/string_tools.py
@@ -1,7 +1,8 @@
 import re
 
 
-def prettify(string):
-    new_string = re.sub('_', ' ', string)
+def prettify(label):
+    label = str(label)
+    new_string = re.sub('_', ' ', label)
     new_string = new_string.capitalize()
     return new_string
