5d7608b6fc45eb2950b703473c2b775ceea6c450
ben.torvaney <ben.torvaney@footballradar.com>
2016-12-05 17:01:59 +0000

    Added setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='chainplot',
+    packages=['chainplot'],
+    version='0.1',
+    description='A(nother) matplotlib wrapper',
+    author='Ben Torvaney',
+    author_email='btorvaney@gmail.com',
+    url='https://github.com/torvaney/chainplot',
+    download_url='https://github.com/torvaney/chainplot/tarball/0.1',
+    keywords=['plot', 'ggplot'],
+    classifiers=[],
+)
