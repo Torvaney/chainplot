a411a2301794ca616a15b4c4fe29f4881904465f
ben.torvaney <ben.torvaney@footballradar.com>
2016-12-05 18:53:17 +0000

    Update requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='chainplot',
@@ -11,5 +10,12 @@ setup(
     url='https://github.com/torvaney/chainplot',
     download_url='https://github.com/torvaney/chainplot/tarball/0.1',
     keywords=['plot', 'ggplot'],
+    install_requires=[
+        'matplotlib>=1.5.3',
+        'numpy>=1.11.2',
+        'pandas>=0.19.1',
+        'scipy>=0.18.1',
+        'adjustText>=0.5.3'
+    ],
     classifiers=[],
 )
